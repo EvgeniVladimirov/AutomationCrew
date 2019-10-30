011704181d0b64df717d28ad8e6ec23de451eef2
Evgeni Vladimirov <evgeny_com@abv.bg>
2019-10-30 23:51:39 +0200

    DropDownsAndCheckboxes

---
+++ src/DropDownsAndCheckboxes.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DropDownsAndCheckboxes {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","C://Selenium//chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://google.bg");
+
+	}
+
+}
